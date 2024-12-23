313cd424f6bf242c6011687d66a8d46f46d79946
Gotiu Bettina <bettinagotiu@gmail.com>
2024-12-23 23:32:48 +0200

    Add MainScreen to handle authentication state
    Add MainScreen to handle authentication state and keep the last logged-in user persistent

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,72 @@
 import 'package:flutter/material.dart';
-//import '../pages/speech_to_text.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'pages/signin_screen.dart'; // Importă pagina de SignIn
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:licenta_app/pages/home_screen.dart';
+import '../pages/save_code.dart';
+import '../pages/signin_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(); // Initialize Firebase
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Speech to Text Demo',
+      title: 'Flutter Auth Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SigninScreen(), // Ecranul inițial este SignInScreen
+      home: const SplashScreen(), // Set SplashScreen as the initial route
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkUserStatus();
+  }
+
+  Future<void> _checkUserStatus() async {
+    // Wait for a moment to simulate splash screen delay
+    await Future.delayed(const Duration(seconds: 2));
+
+    // Check if a user is signed in
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      // Navigate to HomeScreen if user is signed in
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    } else {
+      // Navigate to SigninScreen if no user is signed in
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const SigninScreen()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(), // Show a loading indicator
+      ),
     );
   }
 }
