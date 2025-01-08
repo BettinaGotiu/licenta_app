225e85f74e5183389ca8e858f9a333159bb86f4c
Gotiu Bettina <bettinagotiu@gmail.com>
2025-01-08 22:23:30 +0200

    Detection of occurrence of words - progress

--- lib/pages/results_screen.dart
+++ lib/pages/results_screen.dart
@@ -4,11 +4,15 @@ import 'home_screen.dart';
 class ResultsScreen extends StatelessWidget {
   final String spokenText;
   final double averageWpm;
+  final double withinLimitPercentage;
+  final List<String> foundExpressions;
 
   const ResultsScreen({
     Key? key,
     required this.spokenText,
     required this.averageWpm,
+    required this.withinLimitPercentage,
+    required this.foundExpressions,
   }) : super(key: key);
 
   @override
@@ -41,6 +45,36 @@ class ResultsScreen extends StatelessWidget {
               averageWpm.toStringAsFixed(2),
               style: const TextStyle(fontSize: 16),
             ),
+            const SizedBox(height: 20),
+            const Text(
+              'Percentage of Time Within Selected Pace:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              '${withinLimitPercentage.toStringAsFixed(2)}%',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Common Expressions Found:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            foundExpressions.isEmpty
+                ? const Text(
+                    'No common expressions found.',
+                    style: TextStyle(fontSize: 16),
+                  )
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: foundExpressions.map((expression) {
+                      return Text(
+                        expression,
+                        style: const TextStyle(fontSize: 16),
+                      );
+                    }).toList(),
+                  ),
             const Spacer(),
             Center(
               child: ElevatedButton(
