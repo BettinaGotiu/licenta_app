f0e49e074d67d5f0aa5589af1a0bca8db81e86a0
Gotiu Bettina <bettinagotiu@gmail.com>
2025-02-02 14:50:04 +0200

    Fixed daily streak calendar, now displaying sessions correctly :)

--- lib/pages/calendar_screen.dart
+++ lib/pages/calendar_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class CalendarScreen extends StatefulWidget {
@@ -11,6 +13,42 @@ class CalendarScreen extends StatefulWidget {
 class _CalendarScreenState extends State<CalendarScreen> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+  Set<DateTime> _sessionDates = {}; // Using Set for quick lookup
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchSessionDates();
+  }
+
+  Future<void> _fetchSessionDates() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final snapshot = await FirebaseFirestore.instance
+          .collection('user_data')
+          .doc(user.uid)
+          .collection('sessions')
+          .orderBy('date')
+          .get();
+
+      setState(() {
+        _sessionDates = snapshot.docs.map((doc) {
+          DateTime date = DateTime.parse(doc['date']);
+          return DateTime(date.year, date.month, date.day);
+        }).toSet();
+        _isLoading = false;
+      });
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  bool _isSessionDate(DateTime date) {
+    return _sessionDates.contains(DateTime(date.year, date.month, date.day));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,24 +62,78 @@ class _CalendarScreenState extends State<CalendarScreen> {
           onPressed: () => Navigator.pop(context),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: TableCalendar(
-          firstDay: DateTime.utc(2010, 10, 16),
-          lastDay: DateTime.utc(2030, 3, 14),
-          focusedDay: _focusedDay,
-          selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
-          onDaySelected: (selectedDay, focusedDay) {
-            setState(() {
-              _selectedDay = selectedDay;
-              _focusedDay = focusedDay;
-            });
-          },
-          calendarFormat: CalendarFormat.month, // Larger month view
-          onFormatChanged: (format) {}, // Format change not needed
-          onPageChanged: (focusedDay) => _focusedDay = focusedDay,
-          locale: 'en_US', // Language set to English
-        ),
+      body: FutureBuilder(
+        future: _fetchSessionDates(),
+        builder: (context, snapshot) {
+          if (_isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TableCalendar(
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                focusedDay: _focusedDay,
+                calendarFormat: CalendarFormat.month,
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                },
+                onPageChanged: (focusedDay) {
+                  setState(() {
+                    _focusedDay = focusedDay;
+                  });
+                  _fetchSessionDates();
+                },
+                calendarBuilders: CalendarBuilders(
+                  defaultBuilder: (context, date, _) {
+                    if (_isSessionDate(date)) {
+                      return Container(
+                        margin: const EdgeInsets.all(6.0),
+                        decoration: BoxDecoration(
+                          color: Colors.green, // Always green for session days
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            '${date.day}',
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      );
+                    }
+                    return null;
+                  },
+                  todayBuilder: (context, date, _) {
+                    if (_isSessionDate(date)) {
+                      return Container(
+                        margin: const EdgeInsets.all(6.0),
+                        decoration: BoxDecoration(
+                          color: Colors
+                              .green, // Force green for today if session exists
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            '${date.day}',
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      );
+                    }
+                    return null; // Removes default purple highlight if no session today
+                  },
+                ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
